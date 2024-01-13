3a0ddbd64ae190b5d0f03185c668de62a1b7a291
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-01-13 18:49:05 +0700

    hmm somethings broken

--- lua/ts-node-action/filetypes/javascript.lua
+++ lua/ts-node-action/filetypes/javascript.lua
@@ -21,10 +21,13 @@ local padding = {
 return {
   ["property_identifier"] = actions.cycle_case(),
   ["string_fragment"] = actions.conceal_string(),
+  ["statement_block"] = {
+    { actions.conceal_string(), name = "Conceal" },
+    { actions.toggle_multiline(padding), name = "Split Join" },
+  },
   ["binary_expression"] = actions.toggle_operator(operators),
   ["object"] = actions.toggle_multiline(padding),
   ["array"] = actions.toggle_multiline(padding),
-  ["statement_block"] = actions.toggle_multiline(padding),
   ["object_pattern"] = actions.toggle_multiline(padding),
   ["object_type"] = actions.toggle_multiline(padding),
   ["formal_parameters"] = actions.toggle_multiline(padding),
