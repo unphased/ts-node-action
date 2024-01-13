fa62dd6a2deaf8f1d17cd949b3d9163a7f3ff910
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-01-13 23:44:15 +0700

    this is correct offsets now

--- lua/ts-node-action/actions/cycle_template_string.lua
+++ lua/ts-node-action/actions/cycle_template_string.lua
@@ -4,10 +4,22 @@ return function()
 
   local function action(node)
     local text = helpers.node_text(node)
-    local _, _, startIdx, _, _, endIdx = node:range(true)
-    log("text", text, "from " .. tostring(startIdx) .. " to " .. tostring(endIdx))
+    local _, _, start, _, _, end_ = node:range(true)
+    log("text", text, "from " .. tostring(start) .. " to " .. tostring(end_))
+    local i = 0
+    local last_cend
     for child in node:iter_children() do
-      log("child", child, helpers.node_text(child), child:range(true))
+      i = i + 1
+      local _, _, cstart, _, _, cend = child:range(true)
+      if i > 1 then
+        -- for 2nd and subsequent children, show template string content from prev sibling to self
+        local a = last_cend - start
+        local z = cstart - start
+        local intercalated = text:sub(a + 1, z)
+        log("intercalated", intercalated)
+      end
+      log("child", i, child:type(), helpers.node_text(child))
+      last_cend = cend
     end
     return text
   end
